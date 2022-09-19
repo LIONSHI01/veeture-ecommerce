133401908d85d530d16502b63b5239d6b03c7d16
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-09-19 13:08:28 +0800

    fixed styled-components not render properly bug by modifying the _document.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+    },
+  },
 };
 
 module.exports = nextConfig;
